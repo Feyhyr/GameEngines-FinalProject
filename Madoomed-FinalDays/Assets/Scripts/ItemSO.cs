821256f21723618351150855afe05bfa48fbd2a2
Imawaffle <cchang61@my.centennialcollege.ca>
2020-12-07 02:00:09 -0500

    made ItemSO scripts and prefabs/changes to CynthiaScene

---
+++ Madoomed-FinalDays/Assets/Scripts/ItemSO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Category
+{
+    BodyColour,
+    Hat,
+    Tail,
+    Wings,
+    Neck,
+    Face
+}
+
+[CreateAssetMenu(menuName = "Item")]
+public class ItemSO : ScriptableObject
+{
+    public Sprite icon;
+    public Category category;
+    public string iName;
+}
