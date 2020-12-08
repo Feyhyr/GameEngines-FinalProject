885c62002c36055476c416813fb6c30a4cd6ae2d
Imawaffle <cchang61@my.centennialcollege.ca>
2020-12-07 22:55:46 -0500

    made scripts for game

---
+++ Madoomed-FinalDays/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour
+{
+    public Image body;
+    public Image face;
+    public Image hat;
+    public Image neck;
+    public Image tail;
+    public Image wings;
+}
---
+++ Madoomed-FinalDays/Assets/Scripts/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemController : MonoBehaviour
+{
+    public ItemSO item;
+    public Image itemIcon;
+
+    public Inventory inv;
+
+    /*public void MoveList()
+    {
+        if (transform.parent == inv.invListLocation)
+        {
+            //FindObjectOfType<QuestUtil>().submitBTN.SetActive(true);
+            transform.SetParent(inv.npcListLocation);
+            inv.npcList.Add(item);
+        }
+    }*/
+
+    public void ChangeCharacterPart()
+    {
+        /*if (item.category == ItemSO.Category.BodyColour)
+        {
+            GetComponent<CharacterManager>().body.sprite = item.icon;
+        }*/
+        
+    }
+}
