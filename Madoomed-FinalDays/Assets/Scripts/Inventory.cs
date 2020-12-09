64d8f6a1ea648e0362fed04edd8430e24d380d04
Imawaffle <cchang61@my.centennialcollege.ca>
2020-12-09 02:09:08 -0500

    Big Update for Game

--- Madoomed-FinalDays/Assets/Scripts/Inventory.cs
+++ Madoomed-FinalDays/Assets/Scripts/Inventory.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     public GameObject prefab_Container;
     public Transform invListLocation;
-    public Transform equippedListLocation;
+    public Transform bodyEquippedLocation;
+    public Transform faceEquippedLocation;
+    public Transform neckEquippedLocation;
+    public Transform hatEquippedLocation;
+    public Transform tailEquippedLocation;
+    public Transform wingsEquippedLocation;
     public List<ItemSO> itemList;
-    public List<ItemSO> equippedList;
+    public List<ItemSO> filteredList;
 
     private void Start()
     {
@@ -17,13 +23,44 @@ public class Inventory : MonoBehaviour
 
     public void AddItemToList()
     {
+        GameObject myPrefab;
+
         for (int i = 0; i < itemList.Count; i++)
         {
-            GameObject myPrefab = Instantiate(prefab_Container, invListLocation);
+            if ((itemList[i].iName == "Blue"))
+            {
+                myPrefab = Instantiate(prefab_Container, bodyEquippedLocation);
+            }
+            else if (itemList[i].iName == "Smug Face")
+            {
+                myPrefab = Instantiate(prefab_Container, faceEquippedLocation);
+            }
+            else if (itemList[i].iName == "Cat Ears")
+            {
+                myPrefab = Instantiate(prefab_Container, hatEquippedLocation);
+            }
+            else if (itemList[i].iName == "Soul Gem Pendant")
+            {
+                myPrefab = Instantiate(prefab_Container, neckEquippedLocation);
+            }
+            else if (itemList[i].iName == "Cat Tail")
+            {
+                myPrefab = Instantiate(prefab_Container, tailEquippedLocation);
+            }
+            else if (itemList[i].iName == "Angel Wings")
+            {
+                myPrefab = Instantiate(prefab_Container, wingsEquippedLocation);
+            }
+            else
+            {
+                myPrefab = Instantiate(prefab_Container, invListLocation);
+            }
+
             myPrefab.GetComponent<ItemController>().inv = this;
             myPrefab.GetComponent<ItemController>().item = itemList[i];
             myPrefab.GetComponent<ItemController>().audioSFX = itemList[i].audio;
-            if ((itemList[i].category == ItemSO.Category.BodyColour) || (itemList[i].category == ItemSO.Category.Hat))
+
+            if ((itemList[i].category == ItemSO.Category.BodyColour) || (itemList[i].category == ItemSO.Category.Hat) || (itemList[i].category == ItemSO.Category.Neck) || (itemList[i].category == ItemSO.Category.Wings))
             {
                 myPrefab.GetComponent<ItemController>().itemIcon.sprite = itemList[i].displayIcon;
             }
@@ -32,5 +69,11 @@ public class Inventory : MonoBehaviour
                 myPrefab.GetComponent<ItemController>().itemIcon.sprite = itemList[i].icon;
             }
         }
+    }
+
+    public List<ItemSO> FilterItems(ItemSO.Category category)
+    {
+        filteredList = (from item in itemList where item.category == category select item).ToList();
+        return filteredList;
     }
 }
--- Madoomed-FinalDays/Assets/Scripts/ItemController.cs
+++ Madoomed-FinalDays/Assets/Scripts/ItemController.cs
@@ -16,8 +16,43 @@ public class ItemController : MonoBehaviour
     {
         if (transform.parent == inv.invListLocation)
         {
-            transform.SetParent(inv.equippedListLocation);
-            inv.equippedList.Add(item);
+            if (item.category == ItemSO.Category.BodyColour)
+            {
+                transform.SetParent(inv.bodyEquippedLocation);
+                inv.bodyEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+
+            else if (item.category == ItemSO.Category.Hat)
+            {
+                transform.SetParent(inv.hatEquippedLocation);
+                inv.hatEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+
+            else if (item.category == ItemSO.Category.Tail)
+            {
+                transform.SetParent(inv.tailEquippedLocation);
+                inv.tailEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+
+            else if (item.category == ItemSO.Category.Wings)
+            {
+                transform.SetParent(inv.wingsEquippedLocation);
+                inv.wingsEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+
+            else if (item.category == ItemSO.Category.Face)
+            {
+                transform.SetParent(inv.faceEquippedLocation);
+                inv.faceEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+
+            else if (item.category == ItemSO.Category.Neck)
+            {
+                transform.SetParent(inv.neckEquippedLocation);
+                inv.neckEquippedLocation.GetChild(0).SetParent(inv.invListLocation);
+            }
+            ChangeCharacterPart();
+            PlayAudio();
         }
     }
 
